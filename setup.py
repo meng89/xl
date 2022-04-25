d3ea590c9ee3c2dcd109dbabb217769586b499ea
Chen Meng <observerchan@gmail.com>
2022-04-25 21:43:00 +0800

    .

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+from distutils.util import convert_path
+
+
+NAME = "xl"
+
+main_ns = {}
+ver_path = convert_path('{}/version.py'.format(NAME))
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+DESCRIPTION = 'A Python module to process XML'
+
+
+URL = 'https://github.com/meng89/' + NAME
+
+CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+# with open('requirements.txt') as f:
+#    requirements = f.read().splitlines()
+
+setup(name=NAME,
+      version=main_ns['__version__'],
+      description=DESCRIPTION,
+      include_package_data=True,
+      author='Chen Meng',
+      author_email='ObserverChan@gmail.com',
+      license='MIT',
+      url=URL,
+      packages=[
+          'xl'
+      ],
+      # install_requires=requirements,
+      classifiers=CLASSIFIERS)
