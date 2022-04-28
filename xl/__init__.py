0df53234c31ff459140125baa47f68fd2a2463b4
Chen Meng <observerchan@gmail.com>
2022-04-28 12:53:22 +0800

    .

--- xl/__init__.py
+++ xl/__init__.py
@@ -419,10 +419,12 @@ def _parse_element(text, i, do_strip=False, chars=None, dont_do_tags=None):
                             _s = k.strip(chars)
                         else:
                             _s = k
+                        if not _s:
+                            continue
                         _kid = _unescape(_s, _xml_escape_table)
                     else:
                         _kid = k
-                    e.kids.append(_kid)
+                        e.kids.append(_kid)
 
                 return e, i
 
